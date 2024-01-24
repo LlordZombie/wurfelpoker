4d3a5e62ace54ad0062a6c5cfd53d9ecc180718d
LlordZombie <linus.freistetter@hotmail.com>
2024-01-24 14:17:35 +0100

    aaah committen

--- js/script.js
+++ js/script.js
@@ -1,30 +1,40 @@
 "use strict";
+window.addEventListener("load", next);
 document.addEventListener("DOMContentLoaded", main);
-
+let throwsLeft;
 function main() {
     document.getElementsByTagName("Button")[0].addEventListener("click", roll);
     document.getElementsByTagName("Button")[1].addEventListener("click", next);
 }
-
 function roll() {
     let imgs = document.getElementsByClassName("cube");
     let stays = document.getElementsByTagName("input");
     let imgNums = [1, 1, 1, 1, 1, 1];
-    for (let i = 0; i < imgNums.length; i++) {
-        imgNums[i] *= Math.random() * 6 + 1;
-        if (!stays.item(i).checked) {
-            imgs.item(i).setAttribute("src", `imgs/${Math.floor(imgNums[i])}.jpg`);
+    if (throwsLeft > 0) {
+        for (let i = 0; i < imgNums.length; i++) {
+            stays.item(i).disabled = false;
+            imgNums[i] *= Math.random() * 6 + 1;
+            if (!stays.item(i).checked) {
+                imgs.item(i).setAttribute("src", `imgs/${Math.floor(imgNums[i])}.jpg`);
+            }
+        }
+        throwsLeft--;
+        document.getElementsByTagName("Button")[0].textContent = `Throws left: ${throwsLeft}`;
+        if (throwsLeft === 0) {
+            document.getElementsByTagName("Button")[0].disabled = true;
         }
     }
 }
-
 function next() {
     let imgs = document.getElementsByClassName("cube");
     let stays = document.getElementsByTagName("input");
+    throwsLeft = 3;
+    document.getElementsByTagName("Button")[0].textContent = `Throws left: ${throwsLeft}`;
+    document.getElementsByTagName("Button")[0].disabled = false;
     for (let i = 0; i < imgs.length; i++) {
         imgs.item(i).setAttribute("src", `imgs/6.jpg`);
         stays.item(i).checked = false;
+        stays.item(i).disabled = true;
     }
 }
-
 //# sourceMappingURL=script.js.map
--- ts/script.ts
+++ ts/script.ts
@@ -1,5 +1,6 @@
-window.addEventListener("load",next);
+window.addEventListener("load", next);
 document.addEventListener("DOMContentLoaded", main);
+let throwsLeft: number;
 
 function main() {
     document.getElementsByTagName("Button")[0].addEventListener("click", roll);
@@ -10,19 +11,35 @@ function roll() {
     let imgs: HTMLCollectionOf<Element> = document.getElementsByClassName("cube");
     let stays: HTMLCollectionOf<Element> = document.getElementsByTagName("input");
     let imgNums: number[] = [1, 1, 1, 1, 1, 1];
-    for (let i = 0; i < imgNums.length; i++) {
-        imgNums[i] *= Math.random() * 6 + 1;
-        if (!(stays.item(i) as HTMLInputElement)!.checked) {
-            imgs.item(i)!.setAttribute("src", `imgs/${Math.floor(imgNums[i])}.jpg`);
+    if (throwsLeft > 0) {
+        for (let i = 0; i < imgNums.length; i++) {
+            (stays.item(i) as HTMLInputElement)!.disabled = false;
+            imgNums[i] *= Math.random() * 6 + 1;
+            if (!(stays.item(i) as HTMLInputElement)!.checked) {
+                imgs.item(i)!.setAttribute("src", `imgs/${Math.floor(imgNums[i])}.jpg`);
+            }
         }
+        throwsLeft--;
+        document.getElementsByTagName("Button")[0].textContent = `Throws left: ${throwsLeft}`;
+
+        if (throwsLeft ===0) {
+            (document.getElementsByTagName("Button")[0] as HTMLButtonElement).disabled = true;
+        }
+
     }
 }
 
 function next() {
     let imgs: HTMLCollectionOf<Element> = document.getElementsByClassName("cube");
     let stays: HTMLCollectionOf<Element> = document.getElementsByTagName("input");
+    throwsLeft = 3;
+    document.getElementsByTagName("Button")[0].textContent = `Throws left: ${throwsLeft}`;
+    (document.getElementsByTagName("Button")[0] as HTMLButtonElement).disabled = false;
+
     for (let i = 0; i < imgs.length; i++) {
         imgs.item(i)!.setAttribute("src", `imgs/6.jpg`);
-        (stays.item(i) as HTMLInputElement)!.checked= false;
+        (stays.item(i) as HTMLInputElement)!.checked = false;
+        (stays.item(i) as HTMLInputElement)!.disabled = true;
+
     }
 }
